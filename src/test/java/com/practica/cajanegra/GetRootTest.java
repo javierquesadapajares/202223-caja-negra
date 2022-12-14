86bfec15b041eb079326b74eee706a42360f3c0f
javierquesadapajares <javier.quesada.pajares@alumnos.upm.es>
2022-12-14 19:53:15 +0100

    Tests de GetRoot()

--- src/test/java/com/practica/cajanegra/GetRootTest.java
+++ src/test/java/com/practica/cajanegra/GetRootTest.java
@@ -1,20 +1,42 @@
 package com.practica.cajanegra;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
 import com.binarytree.*;
 
-class GetRootTest {
+public class GetRootTest {
+	private BinaryTree binaryTree;
+	String nombre = "Test1";
+	private BinaryTree binaryTree2;
+	int array[] = {1};
+	
 
 	@BeforeEach
 	void setUp() throws Exception {
+		binaryTree = new BinaryTree<>(nombre);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName ("V1. Nombre = Nodo raiz")
+	public void getRoottestV1() {
+		assertEquals(nombre, binaryTree.getRoot().getContent());
+	}
+	
+	@Test
+	@DisplayName ("NV1. Nombre != Nodo raiz")
+	public void getRoottestNV1() {
+		assertNotEquals ("a", binaryTree.getRoot().getContent());     
+	}
+	
+	@Test
+	@DisplayName ("NV2. Nodo raiz = Nulo")
+	public void getRoottestNV2() {
+		    
 	}
 
 }
