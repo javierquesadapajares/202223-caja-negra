9aa86a2a2d51b6648322404ba9007bce9bf28de5
javierquesadapajares <javier.quesada.pajares@alumnos.upm.es>
2022-12-14 18:31:56 +0100

    creacion directorios

---
+++ src/test/java/com/practica/cajanegra/GetRootTest.java
@@ -0,0 +1,20 @@
+package com.practica.cajanegra;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.binarytree.*;
+
+class GetRootTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
